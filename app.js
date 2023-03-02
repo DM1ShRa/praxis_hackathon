0199787dcf7f2471ec458e92800fd6505b119afd
Darash Mishra <darash.mishra@gmail.com>
2023-03-02 21:31:24 +0530

    updated

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const path = require('path');
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'webfonts')))
 
 app.get('/', (req, res) => {
 
